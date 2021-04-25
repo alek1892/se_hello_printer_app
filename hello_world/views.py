543ed6dba26953226e40c0c12a1b626bbffa593d
alek1892 <alek1892@users.noreply.github.com>
2021-04-25 12:02:10 +0200

    name changer

--- hello_world/views.py
+++ hello_world/views.py
@@ -19,3 +19,17 @@ def index():
 @app.route('/outputs')
 def supported_output():
     return ", ".join(SUPPORTED)
+
+
+@app.route('/name')
+def name_changer():
+    name = request.args.get('name')
+    output = request.args.get('output')
+    if not output:
+        output = PLAIN
+    if name is None:
+        return get_formatted(msg, moje_imie,
+                            output.lower())
+    else:
+        return get_formatted(msg, name,
+                            output.lower())
